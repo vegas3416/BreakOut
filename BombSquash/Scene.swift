ceab06b04e5ee23e2cfa779bbb8be33ec35e7872
Jason Villegas <jaso3570@rackspace.com>
2018-10-18 21:52:00 -0500

    More code

--- BombSquash/Scene.swift
+++ BombSquash/Scene.swift
@@ -17,22 +17,19 @@ class BubbleScene: SKScene {
     
     var lastUpdateTime: TimeInterval = 0
     var dt: TimeInterval = 0
+    var gameTimer: Timer!
     
     var gameSpeed: CGFloat = 10.0
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override init(size: CGSize) {
-        super.init(size: size)
-        anchorPoint = CGPoint(x: 0, y: 1.0)
+    override func didMove(to view: SKView) {
         
+        anchorPoint = CGPoint(x: 0, y: 1.0)
         sceneBackground = SKSpriteNode(color: UIColor.lightGray, size: size)
         sceneBackground.anchorPoint = CGPoint(x: 0, y: 1.0) //Anchors to top left
-        sceneBackground.position = CGPoint(x: 0, y: 0) 
+        sceneBackground.position = CGPoint(x: 0, y: 0)
         sceneBackground.zPosition = -1
         self.addChild(sceneBackground)
+        
     }
 
     override func update(_ currentTime: CFTimeInterval) {
@@ -47,24 +44,40 @@ class BubbleScene: SKScene {
             }
             lastUpdateTime = currentTime
             
-            addBubble()
+            //addBubble()
             dropBubbles()
             removeExcessBubbles()
         }
        
     }
     
+    @objc
     func addBubble() {
-        let bubble = SKShapeNode(circleOfRadius: 6)        //SKSpriteNode(color: UIColor.white, size: CGSize(width: 10, height: 10))
+        //Need to randomly make the bubble sizes
+        
+        //let bubble = SKSpriteNode(imageNamed: IMAGE)  //use this if we want to use an image instead
+
+        
+        let bubble = SKShapeNode(circleOfRadius: CGFloat(arc4random_uniform(25) + 5))
         bubble.fillColor = UIColor.red
         sceneBackground.addChild(bubble)
-        let startPoint = CGPoint(x: size.width/2, y: 0)
-        bubble.position = startPoint
+        var startingPoint = CGPoint(x: 0, y: 0)
+        let randomValue = arc4random_uniform(UInt32(size.width))
+        print("Random: \(randomValue) + BubbleWidth: \(UInt32(bubble.frame.width))")
+        if randomValue < UInt32(bubble.frame.width) {
+            startingPoint = CGPoint(x: bubble.frame.width / 2, y: 0)
+        } else if randomValue + UInt32(bubble.frame.width) > UInt32(size.width) {
+            startingPoint = CGPoint(x: size.width - (bubble.frame.width / 2), y: 0)
+        } else {
+            startingPoint = CGPoint(x: Int(randomValue), y: 0)
+        }
+ 
+        bubble.position = startingPoint
     }
     
     func dropBubbles() {
         for child in sceneBackground.children {
-            let xOffset: CGFloat = CGFloat(arc4random_uniform(20)) - 10.0
+            let xOffset: CGFloat = 0
             let yOffset: CGFloat = gameSpeed
             let newLocation = CGPoint(x: child.position.x + xOffset, y: child.position.y - yOffset)
             let moveAction = SKAction.move(to: newLocation, duration: 0.2)
