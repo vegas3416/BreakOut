86d4c45026ee098f189d57e148f8eec5ba962cd3
Jason Villegas <jaso3570@rackspace.com>
2018-10-20 20:40:50 -0500

    Collision and skphysics setup.

--- BombSquash/Scene.swift
+++ BombSquash/Scene.swift
@@ -19,6 +19,7 @@ class BubbleScene: SKScene {
     var gamePointsDelegate: ScoredPointsDelegate?
     
     var sceneBackground: SKSpriteNode!
+    //let playableRect: CGRect
 
     var lastUpdateTime: TimeInterval = 0
     var dt: TimeInterval = 0
@@ -26,6 +27,24 @@ class BubbleScene: SKScene {
     
     var gameSpeed: CGFloat = 10.0
     var zombieCounter = 0
+    
+    let boy = SKSpriteNode(imageNamed: "boy")
+    let boyMovePtsPerSec: CGFloat = 450.0
+    var velociyOfBoy = CGPoint.zero
+    
+    let door = SKSpriteNode(color: UIColor.red, size: CGSize(width: 50, height: 10))
+    
+//    override init(size: CGSize) {
+//        let playableHeight = size.height
+//        let playableWidth = size.width
+//        playableRect = CGRect( x: 0, y: playableWidth, width: playableWidth, height: playableHeight)
+//        super.init(size: size)
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
 
     override func didMove(to view: SKView) {
 
@@ -36,9 +55,17 @@ class BubbleScene: SKScene {
         sceneBackground.zPosition = -1
         self.addChild(sceneBackground)
         
-        //gameTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(addBubble), userInfo: nil, repeats: true)
+        let border = SKPhysicsBody(edgeLoopFrom: sceneBackground.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+       
+        
+        addBoy()
 
-
+        door.name = "door"
+        door.position = CGPoint(x: (size.width / 2), y: (((-1) * size.height) + 10))
+        addChild(door)
     }
 
     override func update(_ currentTime: CFTimeInterval) {
@@ -56,45 +83,55 @@ class BubbleScene: SKScene {
         //Take care of bubble movement and removal of bubble
         dropBubbles()
         removeExcessBubbles()
-       
+
         checkCollisions()
+       
+    }
+    
+    func addBoy() {
+        
+        boy.name = "boy"
+        boy.size = CGSize(width: 30, height: 30)
+        addChild(boy)
+        boy.position = CGPoint(x: boy.size.width, y: (-1) * boy.size.height + 10)
+        boy.physicsBody = SKPhysicsBody(circleOfRadius: boy.size.width / 2)
+        boy.physicsBody?.applyImpulse(CGVector(dx: 10 , dy: 0))
+
+        boy.physicsBody?.isDynamic = true
+        boy.physicsBody?.friction = 0
+        boy.physicsBody?.restitution = 1
+        boy.physicsBody?.linearDamping = 0
+
     }
 
     func addItem() {
 
-        zombieCounter += 1
         var fallingItem = SKSpriteNode()
+
+        fallingItem = SKSpriteNode(imageNamed: "zombie")
+        fallingItem.name = "zombie"
         let sizeOfItem = CGFloat(arc4random_uniform(91) + 10)
-        
-        if zombieCounter > 5 {
-            fallingItem = SKSpriteNode(imageNamed: "boy")
-            fallingItem.name = "boy"
-            fallingItem.size = CGSize(width: 25, height: 25)
-            fallingItem.zPosition = sizeOfItem
-            zombieCounter = 0
-        } else {
-            fallingItem = SKSpriteNode(imageNamed: "zombie")
-            fallingItem.name = "zombie"
-            let sizeOfItem = CGFloat(arc4random_uniform(91) + 10)
-            fallingItem.size = CGSize(width: sizeOfItem, height: sizeOfItem)
-            fallingItem.zPosition = sizeOfItem
-        }
+        fallingItem.size = CGSize(width: sizeOfItem, height: sizeOfItem)
+        fallingItem.physicsBody = SKPhysicsBody(rectangleOf: fallingItem.size)
+        fallingItem.physicsBody?.isDynamic = false
         
         sceneBackground.addChild(fallingItem)
         
-        //Holder for starting point until actual start is set
+        fallingItem.position = startingPoint(node: fallingItem)
+    }
+    
+    func startingPoint(node: SKSpriteNode) -> CGPoint {
         var startingPoint = CGPoint(x: 0, y: 0)
         let randomValue = arc4random_uniform(UInt32(self.size.width))
-
-        if randomValue < UInt32(fallingItem.frame.width) {
-            startingPoint = CGPoint(x: fallingItem.frame.width / 2, y: 0)
-        } else if randomValue + UInt32(fallingItem.frame.width) > UInt32(size.width) {
-            startingPoint = CGPoint(x: size.width - (fallingItem.frame.width / 2), y: 0)
+        
+        if randomValue < UInt32(node.frame.width) {
+            startingPoint = CGPoint(x: node.frame.width / 2, y: 0)
+        } else if randomValue + UInt32(node.frame.width) > UInt32(size.width) {
+            startingPoint = CGPoint(x: size.width - (node.frame.width / 2), y: 0)
         } else {
             startingPoint = CGPoint(x: Int(randomValue), y: 0)
         }
- 
-        fallingItem.position = startingPoint
+        return startingPoint
     }
     
     func dropBubbles() {
@@ -117,28 +154,18 @@ class BubbleScene: SKScene {
     
     func checkCollisions() {
         var hitZombies: [SKSpriteNode] = []
+
         sceneBackground.enumerateChildNodes(withName: "zombie") { node, _ in
             let zombie = node as! SKSpriteNode
-            let boys = self.sceneBackground.children.filter { $0.name == "boy" }
-            
-            if boys.count == 0 { return }
-            
-            for boy in boys {
-                if zombie.frame.intersects(boy.frame) {
-                    hitZombies.append(zombie)
+
+                if zombie.frame.intersects(self.boy.frame) {
+
+                    print("You hit a boy")
                 }
-            }
 
         }
-        
-        for zombie in hitZombies {
-            print("hit Zombie")
-        }
     }
-    
-    func boyRunsAway(boy: SKSpriteNode) {
-        
-    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
@@ -156,12 +183,8 @@ class BubbleScene: SKScene {
         }
         
         //This is so that only one node is removed if multiple nodes reside in the same location
-        let maxNode = nodes.reduce(nodes[0]) { $0.zPosition > $1.zPosition ? $0 : $1 }
+        let maxNode = nodes.reduce(nodes[0]) { $0.frame.width > $1.frame.width ? $0 : $1 }
         
-        //Don't want to allow user to dismiss boy node
-        if maxNode.name == "boy" {
-            return
-        }
         gamePointsDelegate?.gamePoints(value: Int(maxNode.frame.size.width))
         maxNode.removeFromParent()
     }
