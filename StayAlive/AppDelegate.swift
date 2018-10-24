37be5f450e394587ab0620bdb170cd40d2988ccf
Jason Villegas <jaso3570@rackspace.com>
2018-10-23 23:21:35 -0500

    Added in some app icons just for fun

--- StayAlive/AppDelegate.swift
+++ StayAlive/AppDelegate.swift
@@ -13,13 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
-//        // Override point for customization after application launch.
-//        return true
-//    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    // Override point for customization after application launch.
-    return true
+        // Override point for customization after application launch.
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- StayAlive/Utilities.swift
+++ StayAlive/Utilities.swift
@@ -13,7 +13,7 @@ import UIKit
 
 
 extension UIImage {
-    //func resizeImage(_ dimension: CGFloat, opaque: Bool, contentMode: UIViewContentMode = .scaleAspectFit) -> UIImage {
+    
     func resizeImage(_ dimension: CGFloat, opaque: Bool, contentMode: UIView.ContentMode = .scaleAspectFit) -> UIImage {
         var width: CGFloat
         var height: CGFloat
